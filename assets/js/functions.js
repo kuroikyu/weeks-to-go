9c147dd0a6cb56c28ce551f2dde9f149d22b602c
kuroikyu <kuroikyu+github@gmail.com>
2016-06-25 12:55:22 +0100

    beautify

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,7 +1,11 @@
+// Document ready
 $(function() {
+
     // Go through all rows and columns up to my age
     for (var fYear = 1; fYear < years; fYear++) {
+
         for (var fDay = 1; fDay <= 52; fDay++) {
+
             // Make the delay increment itself to make it look like a trail
             incrementDelay();
             // Set the timeout to add the classes to the td to make the colour change
@@ -11,21 +15,24 @@ $(function() {
 
     // Do the same but for the remaining weeks of the current year
     for (var fDay = 1; fDay <= remainingWeeks; fDay++) {
+
         incrementDelay();
         doSetTimeout(fYear, fDay, delay);
 
         // Add class current to current week to highlight it
         if (fDay == remainingWeeks) {
+
             // Increment the delay to make the animation consistent
             incrementDelay();
+
             // Add the class to the apropiate td
             setTimeout(function() {
                 $('tbody').children('tr:nth-child(' + fYear + ')').children('td:nth-child(' + fDay + ')').addClass('current');
             }, delay);
         }
     }
-
 });
+
 // Starting dates. Today and my date of birth
 var dateToday = new Date();
 var dateBorn = new Date("1989-05-29");
